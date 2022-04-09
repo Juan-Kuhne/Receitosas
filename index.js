d8abeedd3d4cf318f7fe6a38ecadf2ca0c51d0f6
Lucris <juankuhne@hotmail.com>
2022-04-09 15:36:09 -0300

    undo vercel config

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/*
 const express = require("express"); //Import the express dependency
 const app = express(); //Instantiate an express app, the main work horse of this server
 app.use(express.static("public"));
@@ -15,4 +14,3 @@ app.listen(port, () => {
   //server starts listening for any attempts from a client to connect at port: {port}
   console.log(`Now listening on port ${port}`);
 });
-*/
